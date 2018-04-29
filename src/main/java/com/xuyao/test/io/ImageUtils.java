d3101749918ad3ca7c1788e7a8d3132d69acaa2c
xuyao <xuyao0@foxmail.com>
2018-04-29 10:38:24 +0800

    test

--- src/main/java/com/xuyao/test/io/ImageUtils.java
+++ src/main/java/com/xuyao/test/io/ImageUtils.java
@@ -1,41 +1,46 @@
 package com.xuyao.test.io;
 
 import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
-import javax.imageio.stream.MemoryCacheImageInputStream;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.Iterator;
 
 public class ImageUtils {
 
-    public static void main(String[] args) {
+    public static void copyImage(String imageUrl, String path){
+        URL url = null;
+        String imageName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
         try {
-            URL url = new URL("http://111.231.89.17/image/docker.png");
-            URLConnection urlConn = url.openConnection();
-            System.out.println(urlConn.getContentType());
-            BufferedImage read = ImageIO.read(new URL("http://111.231.89.17/image/docker.png"));
+            url = new URL(imageUrl);
             InputStream inputStream = url.openStream();
-
             byte[] b = new byte[1024];
             int reads = 0;
-            FileOutputStream f = new FileOutputStream(new File("C:\\Users\\xuyao\\Desktop\\"+"a.gif"));
+            FileOutputStream f = new FileOutputStream(new File(path + imageName));
             while((reads = inputStream.read(b)) != -1){
                 f.write(b, 0, reads);
             }
             f.close();
-            System.out.println(inputStream);
-            ImageInputStream imageInputStream = new MemoryCacheImageInputStream(inputStream);//ImageIO.createImageInputStream(inputStream);
-            Iterator<ImageReader> imageReaders = ImageIO.getImageReaders(imageInputStream);
-            ImageReader next = imageReaders.next();
-            System.out.println(next.getFormatName());
-            //ImageIO.write(read, "png", new File("C:\\Users\\xuyao\\Desktop\\d.png"));
-//            ImageReader ir = new ImageReader("");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public static boolean copyImage1(String imageUrl, String path){
+        String imageName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+        String suffix = imageName.substring(imageName.lastIndexOf(".") + 1);
+        boolean flag = false;
+        try {
+            BufferedImage read = ImageIO.read(new URL(imageUrl));
+            flag = ImageIO.write(read, suffix, new File(path + imageName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
 }
