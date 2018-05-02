96e68eea06449b439a5b61ee36841ac79e0aa27a
xuyao <xuyao0@foxmail.com>
2018-05-02 23:11:34 +0800

    test

---
+++ src/main/java/com/xuyao/test/thread/SemaphoreTest.java
@@ -0,0 +1,33 @@
+package com.xuyao.test.thread;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreTest {
+
+
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newFixedThreadPool(6);
+        Semaphore semaphore = new Semaphore(4);
+        for(int i = 0; i < 6; i++) {
+            es.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        semaphore.acquire();
+                        System.out.println(Thread.currentThread().getName() + ": start");
+                        Thread.sleep(1000L);
+                        semaphore.release();
+                        System.out.println(Thread.currentThread().getName() + ": end");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+es.shutdown();
+    }
+
+}
