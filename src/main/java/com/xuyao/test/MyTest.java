aad72f120474bc3ccc7ba7f02e573c8aa7ba0b02
xuyao <xuyao0@foxmail.com>
2018-08-04 15:33:18 +0800

    test

--- src/main/java/com/xuyao/test/MyTest.java
+++ src/main/java/com/xuyao/test/MyTest.java
@@ -1,13 +1,11 @@
 package com.xuyao.test;
 
-import com.xuyao.test.proxy.ProxyFactory;
-
 public class MyTest {
 
     public static void main(String[] args) {
-        ProxyFactory pf = new ProxyFactory(new SayServiceImpl());
-        ISayService say = (ISayService) pf.getProxyInstance();
-        say.say("hi, xuyao");
+
     }
+
+
 
 }
