164e9102e520fa30b7a1b57863614560f5fc3afc
xuyao <xuyao0@foxmail.com>
2018-12-19 07:53:51 +0800

    test

--- src/main/java/com/xuyao/test/io/nio/ByteBufferTest.java
+++ src/main/java/com/xuyao/test/io/nio/ByteBufferTest.java
@@ -32,18 +32,37 @@ public class ByteBufferTest {
 
         System.out.println(Arrays.toString(buffer.array()));
 
-        buffer = ByteBuffer.allocate(10);
         //position:0 limit:10 capacity:10
-        buffer.put("hello".getBytes());
+        buffer = ByteBuffer.allocate(10);
+
         //position:5 limit:10 capacity:10
+        buffer.put("hello".getBytes());
+
         System.out.println(buffer + "，" + new String(buffer.array()));
+        //position:0 limit:5 capacity:10
         buffer.flip();
-        //position:0 limit:5 capacity:10
-        System.out.println(buffer + ", " + (char)buffer.get());
+
         //position:1 limit:5 capacity:10
-        buffer.rewind();
-        //position:0 limit:5 capacity:10
         System.out.println(buffer + ", " + (char)buffer.get());
+
+        //position:0 limit:5 capacity:10
+        buffer.rewind();
+
+        System.out.println(buffer + ", " + (char)buffer.get());
+
+        //position:0 limit:10 capacity:10
+        buffer.clear();
+
+        System.out.println(buffer);
+
+        buffer.put("world".getBytes());
+        buffer.flip();
+        buffer.get(); //取出w
+        buffer.get(); //取出o
+        //position:3 limit:10 capacity:10
+        buffer.compact(); //将未取出的rld移动到数组最前端(0-2)，position移动到下一个可写位置(3)，为接下来的写操作准备
+
+        System.out.println(buffer);
 
     }
 
