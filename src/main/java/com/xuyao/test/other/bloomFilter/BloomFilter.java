71b15f63a563379a1589e3cdd61abb395c6820bf
xuyao <xuyao0@foxmail.com>
2018-12-23 11:28:26 +0800

    bloom

--- src/main/java/com/xuyao/test/other/bloomFilter/BloomFilter.java
+++ src/main/java/com/xuyao/test/other/bloomFilter/BloomFilter.java
@@ -2,13 +2,18 @@ package com.xuyao.test.other.bloomFilter;
 
 import com.xuyao.test.encrypt.hash.HashUtil;
 
+import java.util.BitSet;
+
 public class BloomFilter {
 
-    private static int size = 10000000;
+    private static int size = 100000000;
     private static int[] array = new int[size];
+    private static BitSet bitSet = new BitSet(size);
 
     public static void main(String[] args) {
+
         long start = System.currentTimeMillis();
+        long startM = Runtime.getRuntime().totalMemory();
         for (int i = 0; i < size; i++) {
             put(i);
         }
@@ -16,6 +21,7 @@ public class BloomFilter {
         System.out.println(contain(10000));
         System.out.println(contain(1000000));
         System.out.println(contain(10000000));
+        System.out.println(Runtime.getRuntime().totalMemory() - startM);
         System.out.println(System.currentTimeMillis() - start);
     }
 
@@ -24,9 +30,13 @@ public class BloomFilter {
         int hash1 = HashUtil.DEKHash(v);
         int hash2 = HashUtil.DJBHash(v);
         int hash3 = HashUtil.SDBMHash(v);
-        array[hash1 % size] = 1;
-        array[hash2 % size] = 1;
-        array[hash3 % size] = 1;
+        bitSet.set(hash1 % size, true);
+        bitSet.set(hash2 % size, true);
+        bitSet.set(hash3 % size, true);
+
+//        array[hash1 % size] = 1;
+//        array[hash2 % size] = 1;
+//        array[hash3 % size] = 1;
     }
 
     public static boolean contain(int i){
@@ -34,9 +44,13 @@ public class BloomFilter {
         int hash1 = HashUtil.DEKHash(v);
         int hash2 = HashUtil.DJBHash(v);
         int hash3 = HashUtil.SDBMHash(v);
-        if(array[hash1 % size] == 0) return false;
-        if(array[hash2 % size] == 0) return false;
-        if(array[hash3 % size] == 0) return false;
+        if(!bitSet.get(hash1 % size)) return false;
+        if(!bitSet.get(hash2 % size)) return false;
+        if(!bitSet.get(hash3 % size)) return false;
+
+//        if(array[hash1 % size] == 0) return false;
+//        if(array[hash2 % size] == 0) return false;
+//        if(array[hash3 % size] == 0) return false;
         return true;
     }
 
