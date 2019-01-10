ff1b8a4fb10a70eda17208f036bcbed2bd4bc154
yao.xu <yao.xu@saiwangyun.com>
2019-01-10 15:50:31 +0800

    excel

--- src/main/java/com/xuyao/test/file/ExcelUtils.java
+++ src/main/java/com/xuyao/test/file/ExcelUtils.java
@@ -1,6 +1,12 @@
 package com.xuyao.test.file;
 
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.util.NumberToTextConverter;
+
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class ExcelUtils {
@@ -17,6 +23,56 @@ public class ExcelUtils {
 
     public static List<List<String>> readContent(String filePath){
         return readContent(new File(filePath));
+    }
+
+
+    public static String getCellValue(Cell cell) {
+        String cellValue = "";
+        if (cell == null) {
+            return cellValue;
+        }
+        // 判断数据的类型
+        switch (cell.getCellType()) {
+            case Cell.CELL_TYPE_NUMERIC: // 数字
+                //short s = cell.getCellStyle().getDataFormat();
+                if (HSSFDateUtil.isCellDateFormatted(cell)) {// 处理日期格式、时间格式
+                    SimpleDateFormat sdf = null;
+                    // 验证short值
+                    if (cell.getCellStyle().getDataFormat() == 14) {
+                        sdf = new SimpleDateFormat("yyyy/MM/dd");
+                    } else if (cell.getCellStyle().getDataFormat() == 21) {
+                        sdf = new SimpleDateFormat("HH:mm:ss");
+                    } else if (cell.getCellStyle().getDataFormat() == 22) {
+                        sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                    } else {
+                        throw new RuntimeException("日期格式错误!!!");
+                    }
+                    Date date = cell.getDateCellValue();
+                    cellValue = sdf.format(date);
+                }else{
+                    cellValue = NumberToTextConverter.toText(cell.getNumericCellValue());
+                }
+                break;
+            case Cell.CELL_TYPE_STRING: // 字符串
+                cellValue = cell.getStringCellValue();
+                break;
+            case Cell.CELL_TYPE_BOOLEAN: // Boolean
+                cellValue = String.valueOf(cell.getBooleanCellValue());
+                break;
+            case Cell.CELL_TYPE_FORMULA: // 公式
+                cellValue = String.valueOf(cell.getCellFormula());
+                break;
+            case Cell.CELL_TYPE_BLANK: // 空值
+                cellValue = null;
+                break;
+            case Cell.CELL_TYPE_ERROR: // 故障
+                cellValue = "非法字符";
+                break;
+            default:
+                cellValue = "未知类型";
+                break;
+        }
+        return cellValue;
     }
 
 }
