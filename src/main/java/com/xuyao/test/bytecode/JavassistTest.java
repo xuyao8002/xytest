cf2906578aca0d78545a26de8e6468318db1ce07
xuyao <xuyao0@foxmail.com>
2019-03-26 22:16:25 +0800

    javassist

---
+++ src/main/java/com/xuyao/test/bytecode/JavassistTest.java
@@ -0,0 +1,28 @@
+package com.xuyao.test.bytecode;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.CtNewMethod;
+
+public class JavassistTest {
+
+    public static void main(String[] args) throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.makeClass("com.xuyao.test.Hello");
+        CtMethod ctMethod = CtNewMethod.make("public void say(){}", ctClass);
+        ctMethod.insertBefore("System.out.println(\"Hello ^_^\");");
+        ctClass.addMethod(ctMethod);
+
+        //执行方法
+        Class<?> clazz = ctClass.toClass();
+        Object obj = clazz.newInstance();
+        obj.getClass().getMethod("say", new Class[]{}).invoke(obj, new Object[]{});
+
+        //保存文件
+//        ctClass.writeFile("G:\\");
+
+
+    }
+
+}
