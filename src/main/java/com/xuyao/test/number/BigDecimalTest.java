4bfe198d44f4ab2c405bb77aa9fe995ae802f2dd
yao.xu <yao.xu@saiwangyun.com>
2019-05-29 17:37:34 +0800

    BigDecimal

--- src/main/java/com/xuyao/test/number/BigDecimalTest.java
+++ src/main/java/com/xuyao/test/number/BigDecimalTest.java
@@ -4,13 +4,16 @@ package com.xuyao.test.number;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
-public class MyTest {
+public class BigDecimalTest {
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
-        BigDecimal b = new BigDecimal("9.989034");
+        BigDecimal b = new BigDecimal("9.899034");
         b = b.setScale(2, BigDecimal.ROUND_HALF_UP);
         System.out.println(b.toString());
-long end = System.currentTimeMillis();
+        //去除尾部0
+        b = b.setScale(2, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
+        System.out.println(b.toString());
+        long end = System.currentTimeMillis();
         System.out.println(end - start);
 
         start = System.currentTimeMillis();
