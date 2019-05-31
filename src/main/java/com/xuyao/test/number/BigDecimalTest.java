4a1e03bb1c5754fbe782173d9433fd7f7dbb2a15
yao.xu <yao.xu@saiwangyun.com>
2019-05-31 09:53:34 +0800

    bigDecimal比较大小

--- src/main/java/com/xuyao/test/number/BigDecimalTest.java
+++ src/main/java/com/xuyao/test/number/BigDecimalTest.java
@@ -7,7 +7,7 @@ import java.text.DecimalFormat;
 public class BigDecimalTest {
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
-        BigDecimal b = new BigDecimal("9.899034");
+        BigDecimal b = new BigDecimal("9.8990340");
         b = b.setScale(2, BigDecimal.ROUND_HALF_UP);
         System.out.println(b.toString());
         //去除尾部0
@@ -17,12 +17,16 @@ public class BigDecimalTest {
         System.out.println(end - start);
 
         start = System.currentTimeMillis();
-        BigDecimal b1 = new BigDecimal("9.989034");
+        BigDecimal b1 = new BigDecimal("9.899034");
         DecimalFormat df = new DecimalFormat("#.00");
         String format = df.format(b1);
         System.out.println(format);
         end = System.currentTimeMillis();
         System.out.println(end - start);
+
+        //比较两个值是否相等应用compareTo
+        System.out.println(b.compareTo(b1));
+        System.out.println(b.equals(b1));
     }
 }
 
