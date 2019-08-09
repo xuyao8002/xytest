76d6cd716a2ecf4e831f52be51d54746955f6762
xuyao <xuyao0@foxmail.com>
2019-08-09 22:52:41 +0800

    关闭线程池

--- src/main/java/com/xuyao/test/other/rate/GuavaRate.java
+++ src/main/java/com/xuyao/test/other/rate/GuavaRate.java
@@ -2,6 +2,7 @@ package com.xuyao.test.other.rate;
 
 import com.google.common.util.concurrent.RateLimiter;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -10,11 +11,13 @@ public class GuavaRate {
     public static void main(String[] args) throws InterruptedException {
         int threadNum = 10;
         //每秒获取5个
-        RateLimiter rateLimiter = RateLimiter.create(5.0);
+        RateLimiter rateLimiter = RateLimiter.create(5);
         TimeUnit.SECONDS.sleep(1);
         ExecutorService executorService = Executors.newFixedThreadPool(threadNum);
+        CountDownLatch latch = new CountDownLatch(5);
         for(int i = 0; i < threadNum; i++){
             executorService.execute(() -> {
+                latch.countDown();
                 if(rateLimiter.tryAcquire()){
                     System.out.println("获取到");
                 }else{
@@ -22,6 +25,7 @@ public class GuavaRate {
                 }
             });
         }
-
+        latch.await();
+        executorService.shutdown();
     }
 }
