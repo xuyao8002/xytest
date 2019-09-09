abc6520f220454ae94a303f45f4d6ec5c73432ec
xuyao <xuyao0@foxmail.com>
2019-09-10 07:51:40 +0800

    分布式锁 ZooKeeper Curator

--- src/main/java/com/xuyao/test/other/distributedLocks/zooKeeper/MutexLock.java
+++ src/main/java/com/xuyao/test/other/distributedLocks/zooKeeper/MutexLock.java
@@ -7,20 +7,72 @@ import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MutexLock {
 
-    public static void main(String[] args) throws Exception {
-        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
-        CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", retryPolicy);
+    private RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+
+    private String connectString = "localhost:2181";
+
+    private String path = "/mutex/lock";
+
+    private CuratorFramework client;
+
+    private InterProcessMutex mutex;
+
+    public MutexLock() {
+        client = CuratorFrameworkFactory.newClient(connectString, retryPolicy);
         client.start();
+        mutex = new InterProcessMutex(client, path);
+    }
 
-        InterProcessMutex mutex = new InterProcessMutex(client, "/mutex/lock");
+    public void acquire() throws Exception {
         mutex.acquire();
+    }
 
-        System.out.println("mutex lock");
-
+    public void release() throws Exception {
         mutex.release();
+    }
+
+    public void close(){
         client.close();
+    }
+
+    public void mutex() throws Exception {
+        int i = 10;
+        CountDownLatch latch = new CountDownLatch(i);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        for (int i1 = 0; i1 < i; i1++) {
+            executorService.execute(() -> {
+                System.out.println(Thread.currentThread().getName() + "start");
+                MutexLock mutexLock = new MutexLock();
+                try {
+                    mutexLock.acquire();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + ", hello");
+                latch.countDown();
+                try {
+                    mutexLock.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "end");
+                mutexLock.close();
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+    }
+
+    public static void main(String[] args) throws Exception {
+        MutexLock x = new MutexLock();
+        x.mutex();
     }
 
 }
