41de7dad3ad954996bf9e621e34aca21981dfbfa
yao.xu <yao.xu@saiwangyun.com>
2019-11-01 16:00:12 +0800

    RSA增加解密重载方法 增加生成密钥对方法

--- src/main/java/com/xuyao/test/encrypt/RSAUtils.java
+++ src/main/java/com/xuyao/test/encrypt/RSAUtils.java
@@ -1,9 +1,7 @@
 package com.xuyao.test.encrypt;
 
 import javax.crypto.Cipher;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.*;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
@@ -11,6 +9,8 @@ import java.util.Base64;
 public class RSAUtils {
 
     private static final String CHARSET = "UTF-8";
+
+    private static final String ALGORITHM = "RSA";
 
     public static void main(String[] args) throws Exception {
 
@@ -23,21 +23,23 @@ public class RSAUtils {
 
         //私钥
         PrivateKey privateKey=getPrivateKey(priKeyStr);
-
         //加密
         byte[] encryptedBytes=encrypt(data.getBytes(), publicKey);
         System.out.println("加密后："+new String(encryptedBytes));
-
         //解密
         byte[] decryptedBytes=decrypt(encryptedBytes, privateKey);
         System.out.println("解密后："+new String(decryptedBytes));
 
     }
 
+    private static String decrypt(String text, PrivateKey privateKey) throws Exception {
+        return new String(decrypt(Base64.getDecoder().decode(text), privateKey));
+    }
+
     public static PublicKey getPublicKey(String publicKeyStr) throws Exception{
         byte[] keyBytes= Base64.getDecoder().decode(publicKeyStr.getBytes(CHARSET));
         X509EncodedKeySpec keySpec=new X509EncodedKeySpec(keyBytes);
-        KeyFactory keyFactory= KeyFactory.getInstance("RSA");
+        KeyFactory keyFactory= KeyFactory.getInstance(ALGORITHM);
         return keyFactory.generatePublic(keySpec);
     }
 
@@ -45,23 +47,32 @@ public class RSAUtils {
     public static PrivateKey getPrivateKey(String privateKeyStr) throws Exception{
         byte[] keyBytes=Base64.getDecoder().decode(privateKeyStr.getBytes(CHARSET));
         PKCS8EncodedKeySpec keySpec=new PKCS8EncodedKeySpec(keyBytes);
-        KeyFactory keyFactory=KeyFactory.getInstance("RSA");
+        KeyFactory keyFactory=KeyFactory.getInstance(ALGORITHM);
         return keyFactory.generatePrivate(keySpec);
     }
 
 
     public static byte[] encrypt(byte[] content, PublicKey publicKey) throws Exception{
-        Cipher cipher=Cipher.getInstance("RSA");
+        Cipher cipher=Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
         return cipher.doFinal(content);
     }
 
 
     public static byte[] decrypt(byte[] content, PrivateKey privateKey) throws Exception{
-        Cipher cipher=Cipher.getInstance("RSA");
+        Cipher cipher=Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
         return cipher.doFinal(content);
     }
 
+
+    public static void generateKeyPairs() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGen = KeyPairGenerator
+                .getInstance(ALGORITHM);
+        keyPairGen.initialize(1024);
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        System.out.println(Base64Util.encode1(keyPair.getPublic().getEncoded()));
+        System.out.println(Base64Util.encode1(keyPair.getPrivate().getEncoded()));
+    }
 
 }
