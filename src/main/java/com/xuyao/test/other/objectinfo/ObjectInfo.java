46afdcab490c46f82cf3a9a0eb738a863b4957a7
yao.xu <yao.xu@saiwangyun.com>
2019-12-26 09:37:39 +0800

    查看对象布局、大小信息

--- src/main/java/com/xuyao/test/other/objectinfo/ObjectInfo.java
+++ src/main/java/com/xuyao/test/other/objectinfo/ObjectInfo.java
@@ -3,7 +3,7 @@ package com.xuyao.test.other.objectinfo;
 import org.openjdk.jol.info.ClassLayout;
 
 /**
- * 查看对象总局、大小信息
+ * 查看对象布局、大小信息
  * 在HotSpot虚拟机中，对象在内存中存储的布局可以分为三个区域：对象头(Header)、实例数据(Instance Data)、对齐填充(Padding)
  * 对象头固定12字节，Info字段4字节，String字段4字节，加起来不是8的总数，填充4字节，一共24字节
  */
