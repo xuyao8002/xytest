0a621638c7ee7cd25de69338b21bbeff8d8f00a0
yao.xu <yao.xu@saiwangyun.com>
2020-03-13 17:55:52 +0800

    base64工具类整理

--- src/main/java/com/xuyao/test/encrypt/Base64Util.java
+++ src/main/java/com/xuyao/test/encrypt/Base64Util.java
@@ -8,53 +8,39 @@ public class Base64Util {
     private static final String CHARSET = "UTF-8";
     public static String encode(String str) throws Exception {
         BASE64Encoder encoder = new BASE64Encoder();
-        String en = encoder.encode(str.getBytes(CHARSET));
-        System.out.println(en);
-        return en;
+        return encoder.encode(str.getBytes(CHARSET));
     }
 
-    public static String decode(String str) throws Exception {
+    public static byte[] decode(String str) throws Exception {
         BASE64Decoder decoder = new BASE64Decoder();
-        String de = new String(decoder.decodeBuffer(str), CHARSET);
-        System.out.println(de);
-        return de;
+        return decoder.decodeBuffer(str);
     }
 
     //1.8
     public static String encode1(String str) throws Exception {
         java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
-        String en = encoder.encodeToString(str.getBytes(CHARSET));
-        System.out.println(en);
-        return en;
+        return encoder.encodeToString(str.getBytes(CHARSET));
     }
 
     public static String encode1(byte[] bytes) {
         java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
-        String en = encoder.encodeToString(bytes);
-        System.out.println(en);
-        return en;
+        return encoder.encodeToString(bytes);
     }
 
     //1.8
-    public static String decode1(String str) throws Exception {
+    public static byte[] decode1(String str) {
         java.util.Base64.Decoder decoder = java.util.Base64.getDecoder();
-        String de = new String(decoder.decode(str), CHARSET);
-        System.out.println(de);
-        return de;
+        return decoder.decode(str);
     }
 
     public static String encode2(String str) throws Exception {
         org.apache.commons.codec.binary.Base64 base64 = new org.apache.commons.codec.binary.Base64();
-        String en = base64.encodeToString(str.getBytes(CHARSET));
-        System.out.println(en);
-        return en;
+        return base64.encodeToString(str.getBytes(CHARSET));
     }
 
-    public static String decode2(String str) throws Exception {
+    public static byte[] decode2(String str) {
         org.apache.commons.codec.binary.Base64 base64 = new org.apache.commons.codec.binary.Base64();
-        String de = new String(base64.decode(str), CHARSET);
-        System.out.println(de);
-        return de;
+        return base64.decode(str);
     }
 
 }
