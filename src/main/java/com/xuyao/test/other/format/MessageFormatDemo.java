dc6b07ff8d895ea1bb1667c0bc6b94f43fff25df
yao.xu <yao.xu@saiwangyun.com>
2020-04-26 14:44:04 +0800

    MessageFormat json

--- src/main/java/com/xuyao/test/other/format/MessageFormatDemo.java
+++ src/main/java/com/xuyao/test/other/format/MessageFormatDemo.java
@@ -5,8 +5,12 @@ import java.text.MessageFormat;
 public class MessageFormatDemo {
 
     public static void main(String[] args) {
-        String format = MessageFormat.format("hello {0}, my name is {1}", "world", "java");
-        System.out.println(format);
+        String str = MessageFormat.format("hello {0}, my name is {1}", "world", "java");
+        System.out.println(str);
+
+        //json字符串格式化时首尾括号要用'号包围
+        String json = MessageFormat.format("'{'\"name\":\"{0}\"'}'", "honey");
+        System.out.println(json);
     }
 
 }
