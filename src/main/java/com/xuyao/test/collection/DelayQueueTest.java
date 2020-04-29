c17e51286eb14912d551225b77f159a053a01900
xuyao <xuyao0@foxmail.com>
2020-04-29 22:54:52 +0800

    DelayQueue

---
+++ src/main/java/com/xuyao/test/collection/DelayQueueTest.java
@@ -0,0 +1,51 @@
+package com.xuyao.test.collection;
+
+
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+public class DelayQueueTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        DelayQueue<DelayObject> delayQueue = new DelayQueue<>();
+        delayQueue.add(new DelayObject("a", 1000));
+        delayQueue.add(new DelayObject("b", 4000));
+        delayQueue.add(new DelayObject("c", 3000));
+        delayQueue.add(new DelayObject("d", 2000));
+        int size = delayQueue.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println(delayQueue.take());
+        }
+    }
+
+}
+
+
+class DelayObject implements Delayed {
+    private String data;
+    private long startTime;
+
+    public DelayObject(String data, long startTime) {
+        this.data = data;
+        this.startTime = System.currentTimeMillis() + startTime;
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+        return unit.convert(startTime - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public int compareTo(Delayed obj) {
+        return Long.compare(this.startTime, ((DelayObject) obj).startTime);
+    }
+
+    @Override
+    public String toString() {
+        return "DelayObject{" +
+                "data='" + data + '\'' +
+                ", startTime=" + startTime +
+                '}';
+    }
+}
