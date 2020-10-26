157cb08a5ca8c700bdcad2879bc0eb3d2c4e2fbb
yao.xu <yao.xu@saiwangyun.com>
2020-10-26 15:50:57 +0800

    stream list转map

--- src/main/java/com/xuyao/test/function/StreamTest.java
+++ src/main/java/com/xuyao/test/function/StreamTest.java
@@ -1,10 +1,32 @@
 package com.xuyao.test.function;
 
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
 public class StreamTest {
 
-    public static void main(String[] args) {
+    private static void listToMap() {
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> m1 = new HashMap<>();
+        m1.put("name", "xy");
+        m1.put("age", "1");
+        Map<String, Object> m2 = new HashMap<>();
+        m2.put("name", "xy");
+        m2.put("age", "2");
+        Map<String, Object> m3 = new HashMap<>();
+        m3.put("name", "xu");
+        m3.put("age", "3");
+        list.add(m1);
+        list.add(m2);
+        list.add(m3);
+        LinkedHashMap<Object, List<Map<String, Object>>> map = list
+                .stream()
+                .collect(Collectors.groupingBy(m -> m.get("name"), LinkedHashMap::new, Collectors.toList()));
+        map.forEach((x,y) -> System.out.println(x + "," + y));
+    }
+
+    private static void parallel() {
         long sumEnd = 10000000000L;
         long start = System.currentTimeMillis();
         long reduce = LongStream.rangeClosed(0, sumEnd).parallel().reduce(0, Long::sum);
