84bd28a26820a7974ef74a0aa6ae9a691665bce6
xuyao <xuyao0@foxmail.com>
2021-02-24 20:25:38 +0800

    Other

---
+++ src/main/java/com/xuyao/test/other/Other.java
@@ -0,0 +1,14 @@
+package com.xuyao.test.other;
+
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+public class Other {
+    public static void main(String[] args) {
+        System.out.println("Os Name: " + System.getProperty("os.name"));
+        System.out.println("Line Separator is \\r\\n: " + Objects.equals("\r\n", System.getProperty("line.separator")));
+        System.out.println("Line Separator is \\n: " + Objects.equals("\n", System.getProperty("line.separator")));
+        System.out.println("Line Separator is \\r: " + Objects.equals("\r", System.getProperty("line.separator")));
+        System.out.println("Default Charset: " + Charset.defaultCharset());
+    }
+}
