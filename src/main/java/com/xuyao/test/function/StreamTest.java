26bbacd6bb7f6b35258f7c7daa5c9f28f5bb86fc
xuyao <xuyao0@foxmail.com>
2021-11-11 22:39:03 +0800

    stream list转map

--- src/main/java/com/xuyao/test/function/StreamTest.java
+++ src/main/java/com/xuyao/test/function/StreamTest.java
@@ -20,10 +20,17 @@ public class StreamTest {
         list.add(m1);
         list.add(m2);
         list.add(m3);
-        LinkedHashMap<Object, List<Map<String, Object>>> map = list
+        LinkedHashMap<Object, List<Map<String, Object>>> mapList = list
                 .stream()
                 .collect(Collectors.groupingBy(m -> m.get("name"), LinkedHashMap::new, Collectors.toList()));
-        map.forEach((x,y) -> System.out.println(x + "," + y));
+        mapList.forEach((x,y) -> System.out.println(x + "," + y));
+
+        //list转map，无重复key
+        //Map<Object, Map<String, Object>> map = list.stream().collect(Collectors.toMap(m -> m.get("name"), Function.identity()));
+        //Map<Object, Map<String, Object>> map1 = list.stream().collect(Collectors.toMap(m -> m.get("name"), m -> m));
+        //list转map，重复key时新值覆盖旧值
+        Map<Object, Map<String, Object>> map2 = list.stream().collect(Collectors.toMap(m -> m.get("name"), m -> m, (k1, k2) -> k2));
+        System.out.println(map2);
     }
 
     private static void parallel() {
