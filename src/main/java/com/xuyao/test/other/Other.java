e0ea3be8b188e7d1f38c13cee8d07a03ccd3c7f6
xuyao <xuyao0@foxmail.com>
2021-12-16 12:56:18 +0800

    闰年判断

--- src/main/java/com/xuyao/test/other/Other.java
+++ src/main/java/com/xuyao/test/other/Other.java
@@ -11,4 +11,8 @@ public class Other {
         System.out.println("Line Separator is \\r: " + Objects.equals("\r", System.getProperty("line.separator")));
         System.out.println("Default Charset: " + Charset.defaultCharset());
     }
+
+    public static boolean isLeapYear(int year){
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
 }
