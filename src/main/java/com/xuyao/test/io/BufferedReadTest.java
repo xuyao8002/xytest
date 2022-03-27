8b08e060a679074ed0cbc8f3e8bba545ea4fb2a8
xuyao <xuyao0@foxmail.com>
2022-03-27 12:38:50 +0800

    bufferedRead

--- src/main/java/com/xuyao/test/io/BufferedReadTest.java
+++ src/main/java/com/xuyao/test/io/BufferedReadTest.java
@@ -1,19 +1,23 @@
 package com.xuyao.test.io;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.LineIterator;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class BufferedReadTest {
 
-private static String filePath = "E:\\bigData.txt";
+private static String filePath = "E:\\temp.txt";
     public static void main(String[] args) throws Exception {
-read1();
+        read4();
     }
 
     public static void read() throws Exception {
@@ -25,7 +29,7 @@ read1();
     }
 
     public static void read1() throws Exception {
-        FileInputStream is = new FileInputStream(new File(filePath));
+        FileInputStream is = new FileInputStream(filePath);
         Scanner sc = new Scanner(is, "UTF-8");
         int i = 1;
         while (sc.hasNextLine()) {
@@ -34,13 +38,29 @@ read1();
     }
 
     public static void read2() throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
+        BufferedReader br = new BufferedReader(new FileReader(filePath), 1*1024*1024);
         String line;
         int i = 1;
         while ((line = br.readLine()) != null) {
             System.out.println(i + ", " + line);
             i++;
         }
+    }
+
+    public static void read3() throws Exception {
+        try (LineIterator iterator = IOUtils.lineIterator(new FileInputStream(filePath), "UTF-8")) {
+            int i = 1;
+            while (iterator.hasNext()) {
+                String line=iterator.nextLine();
+                System.out.println(i++ + ", " + line);
+            }
+        }
+    }
+
+    public static void read4() throws Exception {
+        Files.lines(Paths.get(filePath), Charset.forName("UTF-8")).forEach(line -> {
+            System.out.println(line);
+        });
     }
 
 }
