30c571dce71adb0a77fc2e41cd8fffc2d29f6ac1
xuyao <xuyao0@foxmail.com>
2025-07-29 18:07:40 +0800

    最大公约数 最小公倍数

--- src/main/java/com/xuyao/test/other/Other.java
+++ src/main/java/com/xuyao/test/other/Other.java
@@ -15,4 +15,15 @@ public class Other {
     public static boolean isLeapYear(int year){
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return (a * b) / gcd(a, b);
+    }
 }
